2aec3afcbdb0c7da2c0cc3fe7e07e8aa8fa79ed1
饶家俊 <raojiajun@bimwinner.com>
2020-04-24 14:44:54 +0800

    app

--- src/App.js
+++ src/App.js
@@ -1,29 +1,60 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {Button, Input, Card, Table} from 'antd';
 import alasql from 'alasql';
 
 const { TextArea } = Input;
 
+
+
+function computeTableColumnData(data){
+  return Object.keys(data).map((key)=>{
+    return {title: key, dataIndex: key, key: 'key'}
+  });
+}
+
 function App() {
+  const [SQLValue, setSQLValue] = useState('');
+  const [tableColumn, setTableColumn] = useState([]);
+  const [tableDataSource, setTableDataSource] = useState([]);
   useEffect(()=>{
     insertData();
   }, [1]);
 
   const insertData = ()=>{
-    alasql("CREATE TABLE students (name string, score number, sex string)");
-    alasql(`insert into students values ('小明', 90, 'male'), ('小红', 91, 'female'), ('小白', 92, 'male')`);
+    alasql("CREATE TABLE students (key string, name string, score number, sex string)");
+    alasql(`insert into students values ('1', '小明', 90, 'male'), ('2', '小红', 91, 'female'), ('3', '小白', 92, 'male')`);
+  };
+
+  const handleExecuteSQL = ()=>{
+    try{
+      let dataSource = alasql(SQLValue);
+      let columns = computeTableColumnData(dataSource[0]);
+
+      setTableColumn(columns);
+      setTableDataSource(dataSource);
+    }catch (e) {
+      alert('sql语法错误')
+    }
+  };
+
+  const handleChangeSQL = (e)=>{
+    setSQLValue(e.target.value)
   };
 
   return (
     <div className="App">
       <Card title="sql 输入框" style={{ width: 600 }}>
-        <TextArea rows={4} />
-        <Button>执行语句</Button>
+        <TextArea rows={4}
+          value={SQLValue}
+          onChange={handleChangeSQL}
+          onPressEnter={handleExecuteSQL}
+        />
+        <Button onClick={handleExecuteSQL}>执行语句</Button>
       </Card>
 
       <Card title="语句结果" style={{ width: 600 }}>
-        <Table />
+        <Table columns={tableColumn} dataSource={tableDataSource}/>
       </Card>
 
     </div>
