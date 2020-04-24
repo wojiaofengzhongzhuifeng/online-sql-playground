9d6866e5fa841c581a5405771a90828f353aff25
饶家俊 <raojiajun@bimwinner.com>
2020-04-24 15:16:44 +0800

    打包

---
+++ build/precache-manifest.1c5bfff1d4e741de3df48b64aef7407a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b632330d17f12fa55c6c48d26d245bc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "730cc488b14da5dff21c",
+    "url": "/static/css/2.57edc79e.chunk.css"
+  },
+  {
+    "revision": "b17b5c43fca8afad41a5",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "730cc488b14da5dff21c",
+    "url": "/static/js/2.12223536.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.12223536.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b17b5c43fca8afad41a5",
+    "url": "/static/js/main.de98869b.chunk.js"
+  },
+  {
+    "revision": "5f477a809787905975e3",
+    "url": "/static/js/runtime-main.3ae8179e.js"
+  }
+]);
