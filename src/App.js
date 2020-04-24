211f3ef7bc9e3f8fe164104abfba5b6cf20b837d
饶家俊 <raojiajun@bimwinner.com>
2020-04-24 13:18:30 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Button} from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+      <Button>test</Button>
+    </div>
+  );
+}
+
+export default App;
